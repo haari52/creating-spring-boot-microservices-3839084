bf0192fe270536dbe2abf5aed8bb02076ce8d828
Harika Thota <harika.thota001@gmail.com>
2024-09-28 10:12:37 +0000

    Repo Changes

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.explorecalijpa.model.Tour;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+}
